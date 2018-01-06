ea554ea7c11db0f58a4ec70d0f0de100918dd1e0
Wuwei Lin <vincentl13x@gmail.com>
2018-01-07 00:58:23 +0800

    Add triangle mesh

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -9,23 +9,58 @@
 #include "mesh.h"
 #include "program.h"
 
-ParticleMesh::ParticleMesh(const std::vector<glm::vec3> &vertices, const std::vector<unsigned short> &indices)
-    : vertices_(vertices), indices_(indices) {
-    assert(glGetError() == 0);
+glm::vec3 TriangleMesh::calcPlaneNormal(unsigned short i1, unsigned short i2, unsigned short i3) const {
+    auto &p1 = vertices_[i1], &p2 = vertices_[i2], &p3 = vertices_[i3];
+    auto v1 = p2 - p1;
+    auto v2 = p3 - p1;
+    auto normal = glm::cross(v1, v2);
+    auto normalized = glm::normalize(normal);
+    return normalized;
+}
 
-    calcNormals();
+void TriangleMesh::calcNormals() {
+    normals_.clear();
+    normals_.resize(vertices_.size(), glm::vec3(0.f));
 
+    for (int i = 0; i < indices_.size(); i += 3) {
+        auto i1 = indices_[i], i2 = indices_[i + 1], i3 = indices_[i + 2];
+        const auto normal = calcPlaneNormal(i1, i2, i3);
+
+        normals_[i1] += normal;
+        normals_[i2] += normal;
+        normals_[i3] += normal;
+    }
+
+    std::transform(normals_.begin(), normals_.end(), normals_.begin(),
+                   [](const glm::vec3 &n) {
+                       auto normal = glm::normalize(n);
+                       return normal;
+                   }
+    );
+}
+
+TriangleMesh::TriangleMesh(const std::vector<glm::vec3> &vertices, const std::vector<unsigned short> &indices)
+    : vertices_(vertices)
+    , indices_(indices) {
     glGenVertexArrays(1, &vao_);
     glBindVertexArray(vao_);
     glGenBuffers(1, &vbo_coord_);
     glGenBuffers(1, &vbo_normal_);
+    glGenBuffers(1, &ebo_);
+
+    assert(glGetError() == 0);
+}
+
+ParticleMesh::ParticleMesh(const std::vector<glm::vec3> &vertices, const std::vector<unsigned short> &indices)
+    : TriangleMesh(vertices, indices) {
+
+    calcNormals();
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo_coord_);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
     glEnableVertexAttribArray(0);
 
     glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3) * vertices_.size(), vertices_.data(), GL_STREAM_DRAW);
-    glGenBuffers(1, &ebo_);
 
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
     glEnableVertexAttribArray(1);
@@ -49,35 +84,5 @@ void ParticleMesh::render(Program &program) {
 
 void ParticleMesh::bind() {
     glBindVertexArray(vao_);
-}
-
-glm::vec3 ParticleMesh::calcPlaneNormal(unsigned short i1, unsigned short i2, unsigned short i3) const {
-    auto &p1 = vertices_[i1], &p2 = vertices_[i2], &p3 = vertices_[i3];
-    auto v1 = p2 - p1;
-    auto v2 = p3 - p1;
-    auto normal = glm::cross(v1, v2);
-    auto normalized = glm::normalize(normal);
-    return normalized;
-}
-
-void ParticleMesh::calcNormals() {
-    normals_.clear();
-    normals_.resize(vertices_.size(), glm::vec3(0.f));
-
-    for (int i = 0; i < indices_.size(); i += 3) {
-        auto i1 = indices_[i], i2 = indices_[i + 1], i3 = indices_[i + 2];
-        const auto normal = calcPlaneNormal(i1, i2, i3);
-
-        normals_[i1] += normal;
-        normals_[i2] += normal;
-        normals_[i3] += normal;
-    }
-
-    std::transform(normals_.begin(), normals_.end(), normals_.begin(),
-                   [](const glm::vec3 &n) {
-                       auto normal = glm::normalize(n);
-                       return normal;
-                   }
-    );
 }
 
--- src/mesh.h
+++ src/mesh.h
@@ -18,29 +18,34 @@ class Mesh {
 public:
     virtual void render(Program &program) = 0;
     virtual void bind() = 0;
+    virtual ~Mesh() = default;
 };
 
-class ParticleMesh : public Mesh {
+class TriangleMesh : public Mesh {
+public:
+    TriangleMesh (const std::vector<glm::vec3> &vertices, const std::vector<unsigned short> &indices);
+
+    glm::vec3 calcPlaneNormal(unsigned short i1, unsigned short i2, unsigned short i3) const;
+    void calcNormals();
+
+
+protected:
+    GLuint vao_;
+    GLuint vbo_coord_, vbo_normal_;
+    GLuint ebo_;
+    std::vector<glm::vec3> vertices_;
+    std::vector<unsigned short> indices_;
+    std::vector<glm::vec3> normals_;
+};
+
+class ParticleMesh : public TriangleMesh {
 public:
     ParticleMesh (const std::vector<glm::vec3> &vertices, const std::vector<unsigned short> &indices);
 
     void render(Program &program) override;
 
     void bind() override;
-
-private:
-    std::vector<glm::vec3> vertices_;
-    std::vector<unsigned short> indices_;
-    std::vector<glm::vec3> normals_;
-
-
-    void calcNormals();
-
-    GLuint vao_;
-    GLuint vbo_coord_, vbo_normal_;
-    GLuint ebo_;
-
-    glm::vec3 calcPlaneNormal(unsigned short i1, unsigned short i2, unsigned short i3) const;
 };
+
 
 #endif //SE344_MESH_H
